1fd866b11f10cb37db7ed05e268ca3b6e8a64150
KennethElijah <kennethelijahcastillo@gmail.com>
2024-12-21 23:35:48 +0800

    Homepage Javascript

--- homepage.js
+++ homepage.js
@@ -15,9 +15,9 @@ const firebaseConfig = {
  
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-
   const auth=getAuth();
   const db=getFirestore();
+  const realTimeDb = getDatabase(app);
 
   onAuthStateChanged(auth, (user)=>{
     const loggedInUserId=localStorage.getItem('loggedInUserId');
